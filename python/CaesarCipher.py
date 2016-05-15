681897dc9cc07b16a2e23fea5d187cdb2c77cdcc
Alan Chu <chu.chuyalan@gmail.com>
2016-05-15 16:11:18 -0400

    encode method can accept letters not in the alphabet without keyerrors

--- python/CaesarCipher.py
+++ python/CaesarCipher.py
@@ -23,35 +23,52 @@ class CaesarCipher(object):
 		self.return_decoded = ""
         
 	def encode(self, str_arg):
-		for letter in str_arg.upper():
-			num = CaesarCipher.abc_dict[letter]
-			# ENCODE - Add the shift value 
-			num += self.shift
-			if num > 26:
-				num -= 26
-			self.encoded_list.append(num)
-		# print CaesarCipher.abc_dict[letter]
-		# print self.encoded_list
+		for letter in str_arg:
+			try: 			
+				num = CaesarCipher.abc_dict[letter.upper()]
+				# ENCODE - Add the shift value 
+				num += self.shift
+				if num > 26:
+					num -= 26
+				self.encoded_list.append(num)
+			except:
+				self.encoded_list.append(letter)
+		# print self.encoded_list ## returns [8, 20, 9, 10, 2, 6, 23, 24, '!']
+
+			# num = CaesarCipher.abc_dict[letter.upper()]
+			# # ENCODE - Add the shift value 
+			# num += self.shift
+			# if num > 26:
+			# 	num -= 26
+			# self.encoded_list.append(num)
+
+		####### Data Type ######
+		# for num in self.encoded_list:
+		# 	print type(num)
+		####### SUPER WEIRD ##########
 		for num in self.encoded_list:
-			self.return_encoded += [letter for letter, number in CaesarCipher.abc_dict.items() if number == num][0]
-		# self.decoded_list = [letter for num in self.encoded_list if  ]
-		return self.return_encoded
+			if type(num) == int:
+				# print [letter for letter, number in CaesarCipher.abc_dict.items() if number == num][0]
+				self.return_encoded += [letter for letter, number in CaesarCipher.abc_dict.items() if number == num][0]
+			else:
+				self.return_encoded += num
+		print self.return_encoded
 
-	def decode(self, str_arg):
-		for letter in str_arg.upper():
-			num = CaesarCipher.abc_dict[letter]
-			# DECODE - subtract the shift value
-			num -= self.shift
-			if num < 1:
-				num += 26
-			self.decoded_list.append(num)
+	# def decode(self, str_arg):
+	# 	for letter in str_arg.upper():
+	# 		num = CaesarCipher.abc_dict[letter]
+	# 		# DECODE - subtract the shift value
+	# 		num -= self.shift
+	# 		if num < 1:
+	# 			num += 26
+	# 		self.decoded_list.append(num)
 
-		for num in self.decoded_list:
-			self.return_decoded += [letter for letter, number in CaesarCipher.abc_dict.items() if number == num][0]
-		return self.return_decoded
+	# 	for num in self.decoded_list:
+	# 		self.return_decoded += [letter for letter, number in CaesarCipher.abc_dict.items() if number == num][0]
+	# 	return self.return_decoded
 
 ## testing ###
 c = CaesarCipher(5); # creates a CipherHelper with a shift of five
-print c.encode('Codewars') # returns 'HTIJBFWX'
-print c.decode('BFKKQJX') # returns 'WAFFLES'
+print c.encode('Code#wars!') # returns 'HTIJBFWX'
+# print c.decode('BFKKQJX') # returns 'WAFFLES'
 # [3, 15, 4, 5, 23, 1, 18, 19]
